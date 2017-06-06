127f8a7180d01bacd3fc486c2bb2d33a823fd9b5
Clara Gaya Saura <clara@Claras-MacBook-Pro.local>
2017-06-06 16:36:14 +0100

    Fix PORT const

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const PORT = process.env.NODE_ENV || 9090;
+const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, (err) => {
     if (err) return console.log('ERROR: ', err);
